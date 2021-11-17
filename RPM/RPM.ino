845f2f2eb346127b09a4d2c212ba870c5b918d6d
jen-tran <jennifert8213@gmail.com>
2021-11-17 04:03:18 -0500

    commenting and adding pinMode

--- RPM/RPM.ino
+++ RPM/RPM.ino
@@ -8,23 +8,24 @@ bool obstacleChange = false;
 int RPM = 0;
 
 bool reset() {
-  Serial.println(RPM);
+  Serial.println(RPM);  // send data
   RPM = 0;
   return true;
 }
 
 void setup() {
   Serial.begin(9700);
+  pinMode(pin, INPUT);
   timer.every(60000, reset);
 }
 
 void loop() {
   timer.tick();
   isObstacle = digitalRead(pin);
-  if (obstacleChange && isObstacle == HIGH) {
-    obstacleChange = !obstacleChange;   
-  } else if (!obstacleChange && isObstacle != HIGH) {
+  if (obstacleChange && isObstacle == HIGH) { // true and 0 (1 to 0)
+    obstacleChange = !obstacleChange; // change obstacleChange back to false  
+  } else if (!obstacleChange && isObstacle != HIGH) { // false and 1 (no obstacle to obstacle)
     RPM++;
-    obstacleChange = !obstacleChange;
+    obstacleChange = !obstacleChange; // change obstacleChange to true
   }
 }
